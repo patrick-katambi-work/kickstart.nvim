e8ddd994e8929a342c1c443db2786db83512dcf2
patrick-simon-katambi <patrick.katambi.work@gmail.com>
2025-05-28 12:10:37 +0300

    nvim tree plugin update

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,11 +1,17 @@
 -- nvim v0.11.1
 return {
-  'nvim-tree/nvim-tree.lua',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons', -- optional for icons
+  {
+    'nvim-tree/nvim-web-devicons',
+    lazy = true,
   },
-  config = function()
-    require('nvim-tree').setup()
-    vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle File Explorer' })
-  end,
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons', -- optional for icons
+    },
+    config = function()
+      require('nvim-tree').setup()
+      vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle File Explorer' })
+    end,
+  },
 }
